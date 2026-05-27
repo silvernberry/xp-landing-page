a2d94c008b765d87450c95c82d75a526ba3787e9
silvernberry <silverberry622@gmail.com>
2026-05-28 02:01:53 +0530

    chore(ci): corrections

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
   url: 'https://silvernberry.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/temp_m3s/pallet-xp/',
+  baseUrl: '/silvernberry/xp-landing-page/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
